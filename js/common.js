a108a0d93b5a2c8c6d449a0e8d9ecb67d9f2fa34
eripo <e205405k@st.u-gakugei.ac.jp>
2022-08-28 00:29:42 +0900

    add necessary file

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(".slider").slick();
+});
+
+// $(".slide-items").slick({
+//     autoplay: false,
+//     dots: false,
+//     arrows: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+// });
+
+// $('.slick01').slick({
+//     autoplay: false,
+//     dots: false,
+//     arrows: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+// });
