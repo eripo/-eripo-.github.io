2184386bff6adcd30059fb807699d1d797147dc2
eripo <e205405k@st.u-gakugei.ac.jp>
2023-03-21 01:49:47 +0900

    update: dTextにswiperとcanvasを組み合わせたものに更新。ただしうまくいっていない。

--- dText copy.js
+++ dText copy.js
@@ -1,63 +1,76 @@
+var flag = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
 
-	/* canvas関係 */
-	const canvas = document.getElementById("myCanvas");
-	const context = canvas.getContext("2d");
+	if(flag === 0) {
+		/* canvas関係 */
+		const canvas = document.getElementById("myCanvas");
+		// const canvas = document.getElementsByClassName("swiper-slide");
+		const context = canvas.getContext("2d");
+		console.log("canvas="+canvas);
+		console.log("context="+context);
 
-	context.rect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = "#fff";	// 白
-	context.fill();
 
-	// 描画線の設定
-	context.lineCap = 'round'; // 丸みを帯びた線にする
-	context.lineJoin = 'round'; // 丸みを帯びた線にする
-	context.lineWidth = 5;
-	context.strokeStyle = "#black";
+		context.rect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = "#fff";	// 白
+		context.fill();
 
-	let mouse = {x: 0, y: 0};
+		// 描画線の設定
+		context.lineCap = 'round'; // 丸みを帯びた線にする
+		context.lineJoin = 'round'; // 丸みを帯びた線にする
+		context.lineWidth = 5;
+		context.strokeStyle = "#black";
 
-	canvas.addEventListener("mousemove", function(e) {
-		mouse.x = e.pageX - this.offsetLeft;
-		mouse.y = e.pageY - this.offsetTop;
-	}, false);
+		let mouse = {x: 0, y: 0};
 
-	canvas.addEventListener("mousedown", function(e) {
-		context.beginPath();
-		context.moveTo(mouse.x, mouse.y);
-	
-	canvas.addEventListener("mousemove", onPaint, false);
-	}, false);
+		canvas.addEventListener("mousemove", function(e) {
+			mouse.x = e.pageX - this.offsetLeft;
+			mouse.y = e.pageY - this.offsetTop;
+		}, false);
 
-	canvas.addEventListener("mouseup", function() {
-		canvas.removeEventListener("mousemove", onPaint, false);
-	})
+		canvas.addEventListener("mousedown", function(e) {
+			context.beginPath();
+			context.moveTo(mouse.x, mouse.y);
+		
+		canvas.addEventListener("mousemove", onPaint, false);
+		}, false);
 
-	const onPaint = function() {
-		context.lineTo(mouse.x, mouse.y);
-		context.stroke();
+		canvas.addEventListener("mouseup", function() {
+			canvas.removeEventListener("mousemove", onPaint, false);
+		})
+
+		const onPaint = function() {
+			context.lineTo(mouse.x, mouse.y);
+			context.stroke();
+		}
+
+
+
+		/* スライダー関係 */
+		// const swiper = new Swiper('.swiper', {
+	  
+		// 	// If we need pagination
+		// 	pagination: {
+		// 	el: '.swiper-pagination',
+		// 	},
+		
+		// 	// Navigation arrows
+		// 	navigation: {
+		// 	nextEl: '.swiper-button-next',
+		// 	prevEl: '.swiper-button-prev',
+		// 	},
+		
+		// 	// // And if we need scrollbar
+		// 	// scrollbar: {
+		// 	//   el: '.swiper-scrollbar',
+		// 	// },
+		// });
 	}
+	
 
 	
 
-	/* スライダー関係 */
-	const swiper = new Swiper('.swiper', {
-	  
-		// If we need pagination
-		pagination: {
-		  el: '.swiper-pagination',
-		},
-	  
-		// Navigation arrows
-		navigation: {
-		  nextEl: '.swiper-button-next',
-		  prevEl: '.swiper-button-prev',
-		},
-	  
-		// And if we need scrollbar
-		scrollbar: {
-		  el: '.swiper-scrollbar',
-		},
-	});
+	
 
 
 	
--- dText.js
+++ dText.js
@@ -1,48 +1,79 @@
-// 2023年3月18日～
+var flag = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
 
-
-	const canvas = document.getElementById("myCanvas");
-	const context = canvas.getContext("2d");
-
-	
-
-	context.rect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = "#fff";
-	context.fill();
-
-	// 描画線の設定
-	context.lineCap = 'round'; // 丸みを帯びた線にする
-	context.lineJoin = 'round'; // 丸みを帯びた線にする
-	context.lineWidth = 5;
-	context.strokeStyle = "#black";
-
-	let mouse = {x: 0, y: 0};
-
-	canvas.addEventListener("mousemove", function(e) {
-		mouse.x = e.pageX - this.offsetLeft;
-		mouse.y = e.pageY - this.offsetTop;
-	}, false);
-
-	canvas.addEventListener("mousedown", function(e) {
-		context.beginPath();
-		context.moveTo(mouse.x, mouse.y);
-	
-	canvas.addEventListener("mousemove", onPaint, false);
-	}, false);
-
-	canvas.addEventListener("mouseup", function() {
-		canvas.removeEventListener("mousemove", onPaint, false);
-	})
+	if(flag === 0) {
+		/* canvas関係 */
+		const canvas = document.getElementById("myCanvas");
+		// const canvas = document.getElementsByClassName("swiper-slide");
+		const context = canvas.getContext("2d");
+		console.log("canvas="+canvas);
+		console.log("context="+context);
 
 
-	const onPaint = function() {
-		context.lineTo(mouse.x, mouse.y);
-		context.stroke();
+		context.rect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = "#fff";	// 白
+		context.fill();
+
+		// 描画線の設定
+		context.lineCap = 'round'; // 丸みを帯びた線にする
+		context.lineJoin = 'round'; // 丸みを帯びた線にする
+		context.lineWidth = 5;
+		context.strokeStyle = "#black";
+
+		let mouse = {x: 0, y: 0};
+
+		canvas.addEventListener("mousemove", function(e) {
+			mouse.x = e.pageX - this.offsetLeft;
+			mouse.y = e.pageY - this.offsetTop;
+		}, false);
+
+		canvas.addEventListener("mousedown", function(e) {
+			context.beginPath();
+			context.moveTo(mouse.x, mouse.y);
+		
+		canvas.addEventListener("mousemove", onPaint, false);
+		}, false);
+
+		canvas.addEventListener("mouseup", function() {
+			canvas.removeEventListener("mousemove", onPaint, false);
+		})
+
+		const onPaint = function() {
+			context.lineTo(mouse.x, mouse.y);
+			context.stroke();
+		}
+
+
+
+		/* スライダー関係 */
+		// const swiper = new Swiper('.swiper', {
+	  
+		// 	// If we need pagination
+		// 	pagination: {
+		// 	el: '.swiper-pagination',
+		// 	},
+		
+		// 	// Navigation arrows
+		// 	navigation: {
+		// 	nextEl: '.swiper-button-next',
+		// 	prevEl: '.swiper-button-prev',
+		// 	},
+		
+		// 	// // And if we need scrollbar
+		// 	// scrollbar: {
+		// 	//   el: '.swiper-scrollbar',
+		// 	// },
+		// });
 	}
+	
+
+	
+
+	
 
 
+	
 
-});
-
+	
+})
