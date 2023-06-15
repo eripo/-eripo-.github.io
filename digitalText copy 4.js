5d9d1195739395c4a6c5f8f170ed21002dd6a580
eripo <e205405k@st.u-gakugei.ac.jp>
2023-06-16 00:58:56 +0900

    fix: copy 4 書き込み座標ずれ修正

--- digitalText copy 4.js
+++ digitalText copy 4.js
@@ -1,10 +1,11 @@
 /***********************  
- * 最終更新日：2023/06/15
+ * 最終更新日：2023/06/16
  ***********************
- * ※変更可 
+ * ※変更可 拡大・縮小機能　大苦戦中
  ***********************
  * デジタル教科書
  * モード切替ボタンでモード変更。
+ * 書き込みの座標ずれ無し。
  *********************** 
  * 
  * startX0: タッチスタート時の座標
@@ -13,11 +14,31 @@
  * 
  * ページめくり幅（endX-startX0）：50
  * 書き込みは、startXからendXの線分を描いていくことで書いている 
+ ***********************
+ * 問題点
+ * ・拡大・縮小不可
+ * 
 ************************/
 
 $(document).ready(function() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
+
+  const drawingCanvas = document.createElement('canvas');
+  const drawingCtx = drawingCanvas.getContext('2d');
+
+  // 要素の位置座標を取得
+  var clientRect = canvas.getBoundingClientRect() ;
+
+  // ページの左端から、要素の左端までの距離
+  var elemGapX = window.pageXOffset + clientRect.left ;
+
+  // ページの上端から、要素の上端までの距離
+  var elemGapY = window.pageYOffset + clientRect.top ;
+
+  console.log('画面左上端の座標: (' + window.pageXOffset + ', ' + window.pageYOffset + ')');
+  console.log('canvas要素の左上端の座標: (' + clientRect.left + ', ' + clientRect.top + ')');
+
 
   let scale = 1; // 現在の拡大率
 
@@ -51,13 +72,13 @@ $(document).ready(function() {
     if (event.type === 'mousedown') {
       startX0 = event.clientX;
       startY0 = event.clientY;
-      startX = event.clientX;
+      startX = event.clientX - elemGapX;
       startY = event.clientY;
     } else if (event.type === 'touchstart') {
       const touch = event.touches[0];
       startX0 = touch.clientX;
       startY0 = touch.clientY;
-      startX = touch.clientX;
+      startX = touch.clientX - elemGapX;
       startY = touch.clientY;
     }
     
@@ -81,9 +102,13 @@ $(document).ready(function() {
     if (isDrawing && mode===1) {
       const page = pages[currentPage - 1];
 
-      const previousX = startX / scale;
+      // const previousX = startX / scale - elemGapX;
+      // const previousY = startY / scale - elemGapY;
+      // const currentX = endX / scale - elemGapX;
+      // const currentY = endY / scale - elemGapY;
+      const previousX = (startX / scale);
       const previousY = startY / scale;
-      const currentX = endX / scale;
+      const currentX = (endX / scale) - elemGapX;
       const currentY = endY / scale;
 
       page.drawings.push({ previousX, previousY, currentX, currentY });
@@ -91,6 +116,10 @@ $(document).ready(function() {
       ctx.beginPath();
       ctx.moveTo(previousX, previousY);
       ctx.lineTo(currentX, currentY);
+      console.log("start: " + startX + ", " + startY);
+      console.log("gap: " + elemGapX + ", " + elemGapY);
+      console.log("previous: " + previousX + ", " + previousY);
+      console.log("current: " + currentX + ", " + currentY);
       ctx.stroke();
 
       startX = currentX;
@@ -189,31 +218,38 @@ $(document).ready(function() {
   });
 
   function updateCanvas() {
-    // clearCanvas();
-    // ctx.save();
-    // ctx.scale(scale, scale);
-    // ctx.drawImage(pages[0], 0, 0);
-    // ctx.restore();
-    // drawPage(currentPage);
-
     const originalWidth = canvas.width;
     const originalHeight = canvas.height;
-
+  
     const scaledWidth = originalWidth * scale;
     const scaledHeight = originalHeight * scale;
-
+  
     tempCanvas.width = scaledWidth;
     tempCanvas.height = scaledHeight;
-
+  
     tempCtx.clearRect(0, 0, scaledWidth, scaledHeight);
     tempCtx.drawImage(canvas, 0, 0, originalWidth, originalHeight, 0, 0, scaledWidth, scaledHeight);
-
+  
     ctx.clearRect(0, 0, originalWidth, originalHeight);
     ctx.drawImage(backgroundImage, 0, 0);
     ctx.drawImage(tempCanvas, 0, 0);
-
-    drawPage(currentPage);
+  
+    // 描画データを反映する
+    const page = pages[currentPage - 1];
+    for (let i = 0; i < page.drawings.length; i++) {
+      const drawing = page.drawings[i];
+      const previousX = drawing.previousX * scale;
+      const previousY = drawing.previousY * scale;
+      const currentX = drawing.currentX * scale;
+      const currentY = drawing.currentY * scale;
+  
+      ctx.beginPath();
+      ctx.moveTo(previousX, previousY);
+      ctx.lineTo(currentX, currentY);
+      ctx.stroke();
+    }
   }
+  
 
 
   const pages = [
@@ -241,14 +277,26 @@ $(document).ready(function() {
     backgroundImage.src = page.background;
   
     backgroundImage.onload = function() {
-      // 画像が読み込まれた後に描画を行う
+      // 背景画像の縮小後の幅と高さを計算する
+      const scaledWidth = canvas.width * scale;
+      const scaledHeight = canvas.height * scale;
+
+      // 背景画像をキャンバスに描画する
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(backgroundImage, 0, 0);
-  
-      // ページの描画内容を反映する（必要な処理を追加）
+      ctx.drawImage(backgroundImage, 0, 0, scaledWidth, scaledHeight);
+
+      // ページの描画内容を反映する
       for (let i = 0; i < page.drawings.length; i++) {
         const drawing = page.drawings[i];
-        // 描画処理の実装
+        const previousX = drawing.previousX * scale;
+        const previousY = drawing.previousY * scale;
+        const currentX = drawing.currentX * scale;
+        const currentY = drawing.currentY * scale;
+      
+        ctx.beginPath();
+        ctx.moveTo(previousX, previousY);
+        ctx.lineTo(currentX, currentY);
+        ctx.stroke();
       }
     };
   }
