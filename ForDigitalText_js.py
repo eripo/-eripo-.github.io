6278175c05f66fb76fb66329682c77ffaf288a8a
eripo <e205405k@st.u-gakugei.ac.jp>
2023-10-06 23:46:21 +0900

    jsで機械学習しようと思ったが断念

---
+++ ForDigitalText_js.py
@@ -0,0 +1,185 @@
+#############################################
+# 学習データとテストデータを入れると
+# 機械学習し、正解率を求めるプログラム
+# js用
+#############################################
+
+# k-近傍法
+
+import numpy as np
+# from keras.datasets import mnist
+# import time
+# import matplotlib.pyplot as plt
+# from PIL import Image
+# from sklearn.model_selection import train_test_split
+# import glob
+# import argparse
+import pandas as pd
+# import japanize_matplotlib
+import pickle
+import tensorflowjs as tfjs
+
+
+import csv
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+
+# 学習用データ #
+df = pd.read_csv( 'Data/learn0_20230822_F_E_502.csv' )
+print(df)
+
+# 説明変数leran_data(特徴量3つ)と目的変数leran_label(判別結果)に分ける
+## 説明変数 適宜不要な列を削除する。削除する列を配列で指定する。################################
+
+# learn用 ##############################
+# # 速度、速度/msec
+# learn_data = df.drop(['aX', 'aY', 'a', 'pos_x', 'pos_y', 'msec', 'aX/msec', 'aY/msec', 'a/msec', 'Mode'], axis=1)
+# # 速度
+# learn_data = df.drop(['aX', 'aY', 'a', 'pos_x', 'pos_y', 'msec', 'v_x/msec', 'v_y/msec', 'v/msec', 'aX/msec', 'aY/msec', 'a/msec', 'Mode'], axis=1)
+# # 速度/msec
+# learn_data = df.drop(['v_x', 'v_y', 'v', 'aX', 'aY', 'a', 'pos_x', 'pos_y', 'msec', 'aX/msec', 'aY/msec', 'a/msec', 'Mode'], axis=1)
+# # 加速度、加速度/msec 
+# learn_data = df.drop(['v_x', 'v_y', 'v', 'pos_x', 'pos_y', 'msec', 'v_x/msec', 'v_y/msec', 'v/msec', 'Mode'], axis=1)
+# # 加速度
+# learn_data = df.drop(['v_x', 'v_y', 'v', 'pos_x', 'pos_y', 'msec', 'aX/msec', 'aY/msec', 'a/msec', 'v_x/msec', 'v_y/msec', 'v/msec', 'Mode'], axis=1)
+# # 加速度/msec 
+# learn_data = df.drop(['v_x', 'v_y', 'v', 'aX', 'aY', 'a', 'pos_x', 'pos_y', 'msec', 'v_x/msec', 'v_y/msec', 'v/msec', 'Mode'], axis=1)
+
+# learn0用 #############################
+# # 筆圧、速度[px/ms]、加速度
+# learn_data = df.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'Mode'], axis=1)
+# # 速度[px/ms]、加速度
+# learn_data = df.drop(['pressure0', 'interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'Mode'], axis=1)
+# 筆圧、速度[px/ms]
+learn_data = df.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'aX', 'aY', 'a', 'Mode'], axis=1)
+# # 筆圧、加速度
+# learn_data = df.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'v_x', 'v_y', 'v', 'Mode'], axis=1)
+# # 筆圧
+# learn_data = df.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'v_x', 'v_y', 'v', 'aX', 'aY', 'a', 'Mode'], axis=1)
+# # 速度[px/ms]
+# learn_data = df.drop(['pressure0', 'interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'aX', 'aY', 'a', 'Mode'], axis=1)
+# # 加速度
+# learn_data = df.drop(['pressure0', 'interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'v_x', 'v_y', 'v', 'Mode'], axis=1)
+###########################################################################################
+learn_label = df['Mode']
+
+print(learn_data)
+print(learn_label)
+
+
+
+print("てすとてすと")
+
+# テストデータ
+df2 = pd.read_csv( 'Data/test0_20230822_F_E_487.csv' )
+print(df2)
+# 説明変数leran_data(特徴量3つ)と目的変数leran_label(判別結果)に分ける
+## 説明変数 適宜不要な列を削除する。削除する列を配列で指定する。################################
+
+# test用 ##############################
+# # test用   速度、加速度、速度/msec、加速度/msec 
+# test_data = df2.drop(['pos_x', 'pos_y', 'msec', 'Mode'], axis=1)
+
+# test0用 #############################
+# # 筆圧、速度[px/ms]、加速度
+# test_data = df2.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'Mode'], axis=1)
+# # 速度[px/ms]、加速度
+# test_data = df2.drop(['pressure0', 'interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'Mode'], axis=1)
+# 筆圧、速度[px/ms]
+test_data = df2.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'aX', 'aY', 'a', 'Mode'], axis=1)
+# # 筆圧、加速度
+# test_data = df2.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'v_x', 'v_y', 'v', 'Mode'], axis=1)
+# # 筆圧
+# test_data = df2.drop(['interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'v_x', 'v_y', 'v', 'aX', 'aY', 'a', 'Mode'], axis=1)
+# # 速度[px/ms]
+# test_data = df2.drop(['pressure0', 'interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'aX', 'aY', 'a', 'Mode'], axis=1)
+# # 加速度
+# test_data = df2.drop(['pressure0', 'interval', 'gapX', 'gapY', 'gap', 'pos_x', 'pos_y', 'msec', 'v_x', 'v_y', 'v', 'Mode'], axis=1)
+###########################################################################################
+test_label = df2['Mode']
+
+print(test_data)
+print(test_label)
+
+
+ 
+# アルゴリズムを指定。K最近傍法を採用
+model = KNeighborsClassifier(n_neighbors=1)
+
+# 学習用のデータと結果を学習する,fit()
+model.fit(learn_data, learn_label)
+
+# 学習モデルの保存
+tfjs.converters.save_keras_model(model, "./my_model")
+
+
+# テストデータによる予測,predict()
+# test_data = [[-63, -21, 66]]
+result_label = model.predict(test_data)
+
+
+# テスト結果を評価する,accuracy_score()
+print("学習用データ：", learn_data)
+print("予測対象：\n", test_data, ", \n予測結果→", result_label)
+print("正解率＝", accuracy_score(test_label, result_label))
+
+
+
+
+
+
+# (https://qiita.com/hikaru_/items/3d64af35769235471d9c)
+# # データ加工・処理・分析モジュール
+# import numpy as np
+# import numpy.random as random
+# import scipy as sp
+# import pandas as pd
+# from pandas import Series, DataFrame
+
+# # 学習用データとテストデータに分けるためのモジュール（正解率を出すため）
+# from sklearn.model_selection import train_test_split
+# # アヤメの花(学習するデータ)
+# from sklearn.datasets import load_iris
+
+# # アヤメの花データ(150個)
+# iris = load_iris()
+# # irisをDataFrameで扱う。
+# df = pd.DataFrame(iris.data, columns=iris.feature_names)
+# # アヤメの種別(ラベル)を追加
+# df["target"] = iris.target_names[iris.target]
+
+# # 引数で表示数を変更できます。defaultは5
+# df.head()
+
+# # 説明変数X(特徴量4つ×150)と目的変数Y(アヤメの種類×150)に分ける
+# X = df.drop('target', axis=1)
+# Y = df['target']
+
+# #ここから学習用データとテスト用データに分ける。random_stateは乱数を固定
+# X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=0)
+
+
+
+# # k-近傍法（k-NN）
+# from sklearn.neighbors import KNeighborsClassifier
+
+# #k-NNインスタンス。今回は3個で多数決。3の値を変更して色々試すと〇
+# model = KNeighborsClassifier(n_neighbors=3)
+# #学習モデル構築。引数に訓練データの特徴量と、それに対応したラベル
+# model.fit(X_train, y_train)
+
+# # .scoreで正解率を算出。
+# print("train score:",model.score(X_train,y_train))
+# print("test score:",model.score(X_test,y_test))
+
+# # 上記データ
+# data = [[5.2, 3.0, 1.5, 0.6]]
+# # data = pd.DataFrame(data)
+# # データフレーム型に変換する際に、行の名前を指定
+# data = pd.DataFrame(data,columns = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"])
+
+# # 構築したモデルからアヤメの種類を求める
+# model.predict(data)
+
---
+++ digitalText_AutoChange_js.js
@@ -0,0 +1,499 @@
+/***********************  
+ * 最終更新日：2023/10/05
+ ***********************
+ * ※変更可 ダメだ―。digitalText_AutoChange_js.jsをコピーしてやり直したほうがよい。
+ ***********************
+ * 判別機能つきデジタル教科書
+ * 
+ ** 機能 **
+ * 書き込み
+ * ページめくり 
+ * モード切替ボタンでモード変更。
+ * 書き込みの座標ずれ無し。
+ * データ取得機能（座標、速度、加速度、インターバルタイム、速度取得間隔）
+ *********************** 
+ * 
+ * startX0: タッチスタート時の座標
+ * startX: ドラッグ中の座標（その区間における始点）
+ * previousX: 1個前の点の座標
+ * currentX: ドラッグ中の現在の座標（その区間における終点）
+ * startTime: インターバルタイム開始時（前回ドラッグ終了時）
+ * endTime: インターバルタイム終了時（次回ドラッグ開始時）
+ * gapX: 前回座標との差
+ * velX: 速さ
+ * subPrevX: 前回座標保存用
+ * positionPrevX: 前回座標
+ * preVelX: 前回速度
+ * 
+ * ページめくり幅（currentX-startX0）：30
+ * 書き込みは、previousXからcurrentXの線分を描いていくことで書いている 
+ ***********************
+ * 問題点・未実装
+ * ・拡大・縮小不可（ボタンのみ有）
+************************/
+
+// JavaScript
+import * as tf from '@tensorflow/tfjs';
+const model = await tf.loadLayersModel('./my_model/model.json');
+
+
+$(document).ready(function() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const drawingCanvas = document.createElement('canvas');
+  const drawingCtx = drawingCanvas.getContext('2d');
+
+  // 要素の位置座標を取得
+  var clientRect = canvas.getBoundingClientRect() ;
+
+  // ページの左端から、canvasの左端までの距離
+  var elemGapX = window.pageXOffset + clientRect.left ;
+
+  // ページの上端から、canvasの上端までの距離
+  var elemGapY = window.pageYOffset + clientRect.top ;
+
+  // console.log('画面左上端の座標: (' + window.pageXOffset + ', ' + window.pageYOffset + ')');
+  // console.log('canvas要素の左上端の座標: (' + clientRect.left + ', ' + clientRect.top + ')');
+
+
+  let scale = 1; // 現在の拡大率
+  var str = "";
+  str += "gapX" + "," + "gapY" + "," + "gap" + "," + "v_x" + "," + "v_y" + "," + "v" + "," + "aX" + "," + "aY" + "," + "a" + "," + "pos_x" + "," + "pos_y" + "," + "msec" + "," + "Mode" + "\n";  // 速度X成分、速度Y成分、合成速度、筆圧
+
+  var str0 = "";
+  str0 += "pressure0" + "," + "interval" + "," + "gapX" + "," + "gapY" + "," + "gap" + "," + "v_x" + "," + "v_y" + "," + "v" + "," + "aX" + "," + "aY" + "," + "a" + "," + "pos_x" + "," + "pos_y" + "," + "msec" + "," + "Mode" + "\n";  // 初速度X成分、初速度Y成分、合成初速度、初筆圧
+
+
+  let count = 0;
+  let isDrawing = false;
+  var isDrag = false;
+
+  let mode = "page";
+  let startX0;
+  let startY0;
+  // let startX = 0;
+  // let startY = 0;
+  let startTime = 0;
+  // let currentX = 0;
+  // let currentY = 0;
+
+  let subPrevX;
+  let subPrevY;
+  let previousX;
+  let previousY;
+  let currentX;
+  let currentY;
+  let pressure;
+
+  // 一時的なキャンバスを作成
+  const tempCanvas = document.createElement('canvas');
+  const tempCtx = tempCanvas.getContext('2d');
+
+  const backgroundImageUrl = '/img/Textbook_page1.png';
+  const backgroundImage = new Image();
+  backgroundImage.src = backgroundImageUrl;
+
+  // 背景画像が読み込まれた後に描画を開始する
+  backgroundImage.onload = function() {
+    drawPage(currentPage);
+  };
+
+
+
+  $(canvas).on('mousedown touchstart', function(event) {
+    // 次回ドラッグ開始時（インターバル終了）
+    endTime = performance.now();
+    previousTime = endTime;
+    // console.log("intervalTime：" + (endTime-startTime) + "\nstart：" + startTime + "\nend：" + endTime)
+
+    event.preventDefault();
+    isDrag = true;
+
+    if (event.type === 'mousedown') {
+      startX0 = event.clientX - elemGapX;
+      startY0 = event.clientY - elemGapY;
+      // startX = event.clientX - elemGapX;
+      // startY = event.clientY - elemGapY;
+      pressure = 1;
+    } else if (event.type === 'touchstart') {
+      const touch = event.touches[0];
+      startX0 = touch.clientX - elemGapX;
+      startY0 = touch.clientY - elemGapY;
+      // startX = touch.clientX - elemGapX;
+      // startY = touch.clientY - elemGapY;
+      pressure = touch.force;
+    }
+    
+    isDrawing = true;
+    gapX = startX0;
+    gapY = startY0;
+    subPrevX = startX0;
+    subPrevY = startY0;
+    previousX = startX0;
+    previousY = startY0;
+    currentX = startX0;
+    currentY = startY0;
+    preVelX = 0;
+    preVelY = 0;
+    preVel = 0;
+    accelerationX = 0;
+    accelerationY = 0;
+    acceleration = 0;
+
+    //1個前の座標（50ミリ秒ごと） 
+    positionPrevX = startX0;
+    positionPrevY = startY0;
+
+    console.log("ドラッグ始点座標: (" + startX0 + ", " + startY0 + ")");
+    // console.log("start: (" + startX + ", " + startY + ")");
+
+    
+    str0 += pressure + "," + (endTime-startTime);
+    // console.log("圧力："+ pressure);
+    speedCount();
+  });
+
+
+  $(canvas).on('mousemove touchmove', function(event) {
+    event.preventDefault();
+
+    if (event.type === 'mousemove') {
+      currentX = event.clientX - elemGapX;
+      currentY = event.clientY - elemGapY;
+    } else if (event.type === 'touchmove') {
+      const touch = event.touches[0];
+      currentX = touch.clientX - elemGapX;
+      currentY = touch.clientY - elemGapY;
+    }
+
+
+    const page = pages[currentPage - 1];
+
+    previousX = subPrevX / scale;
+    previousY = subPrevY / scale;
+    currentX = (currentX / scale);
+    currentY = (currentY / scale);
+
+    // ドラッグ始点の座標
+    console.log("各座標\nstart0\nX座標："+ startX0 +"\nY座標："+ startY0);
+    // 1個前の座標
+    console.log("previous\nX座標："+ previousX +"\nY座標："+ previousY);
+    // 現在の座標
+    console.log("current\nX座標："+ currentX +"\nY座標："+ currentY);
+
+
+    if (isDrawing && mode==="pen") {
+      page.drawings.push({ previousX, previousY, currentX, currentY });
+
+      ctx.beginPath();
+      ctx.moveTo(previousX, previousY);
+      ctx.lineTo(currentX, currentY);
+      // console.log("start: " + startX + ", " + startY);
+      // console.log("gap: " + elemGapX + ", " + elemGapY);
+      // console.log("previous: " + previousX + ", " + previousY);
+      // console.log("current: " + currentX + ", " + currentY);
+      ctx.stroke();
+
+      
+      // previousX = currentX;
+      // previousY = currentY;
+    }
+    subPrevX = currentX;
+    subPrevY = currentY;
+
+
+  });
+
+  $(canvas).on('mouseup touchend', function(event) {
+    // 前回ドラッグ終了時（インターバル開始）
+    startTime = performance.now();
+    
+
+    event.preventDefault();
+    isDrag = false;
+
+    if (event.type === 'mouseup') {
+      currentX = event.clientX - elemGapX;
+      currentY = event.clientY - elemGapY;
+    } else if (event.type === 'touchend') {
+      const touch = event.changedTouches[0];
+      currentX = touch.clientX - elemGapX;
+      currentY = touch.clientY - elemGapY;
+    }
+    isDrawing = false;
+
+
+    // console.log("end-start: " + (currentX-startX0));
+    if(mode==="page") {
+      if (currentX - startX0 < -30) {
+        nextPage();
+      } else if (currentX - startX0 > 30) {
+        prevPage();
+      }
+    }
+
+    count = 0;
+  });
+
+
+
+  function speedCount() {
+    // 速度を計算する処理を記述
+    // if (vel == false) { // pointに値が入ってなかったら、速さ(0,0)
+    //   gapX = 0;
+    //   gapY = 0;
+    // } else {
+    //   if (dpoint0 == false) {
+    //     dpoint0 = point;
+    //   }
+    if(!isDrag) {
+      return;
+    }
+
+    gapX = currentX - positionPrevX;
+    gapY = currentY - positionPrevY;
+    // console.log("gapX,gapY: " + gapX + ", " + gapY + "," + currentX + previousX);
+    // 普通の速さ
+    gap = Math.sqrt(gapX**2 + gapY**2);
+    
+    nowTime = performance.now();
+    msec = nowTime - previousTime;
+
+    previousTime = nowTime;
+    // console.log("msec: " + msec);
+
+    velX = gapX / msec;
+    velY = gapY / msec;
+    vel = gap / msec;
+
+    accelerationX = (velX - preVelX) / msec;
+    accelerationY = (velY - preVelY) / msec;
+    acceleration = (vel - preVel) / msec;
+
+    console.log("print\n" + gapX + ", " + gapY + "," + currentX + ", " + positionPrevX + "," + currentY + ", " + positionPrevY);
+    if(count > 1) {
+      str += gapX + "," + gapY + "," + gap + "," + velX + "," + velY + "," + vel + "," + accelerationX + "," + accelerationY + "," + acceleration + "," + currentX + "," + currentY + "," + msec + "," + mode +"\n";
+    }
+    if(count === 1) {
+
+      const prediction = model.predict([[pressure, velX, velY, vel]]);
+      console.log("Prediction: " + prediction);
+
+      //   $(function(){
+      //     console.log("11111111111111111111111111111111\n" + pressure)
+        
+      //     $.ajax({
+      //         url: 'cgi-bin/open_model.py',
+      //         type: 'get',
+      //         data: {
+      //           test1: pressure,
+      //           test2: velX,
+      //           test3: velY,
+      //           test4: vel
+      //         }
+      //     }).done(function(data){
+      //         console.log("result" + data);
+      //     }).fail(function(){
+      //         console.log('failed');
+      //     });
+      // });
+
+      str0 += "," + gapX + "," + gapY + "," + gap + "," + velX + "," + velY + "," + vel + "," + accelerationX + "," + accelerationY + "," + acceleration + "," + currentX + "," + currentY + "," + (nowTime-endTime) + "," + mode + "\n";
+      str += gapX + "," + gapY + "," + gap + "," + velX + "," + velY + "," + vel + "," + accelerationX + "," + accelerationY + "," + acceleration + "," + currentX + "," + currentY + "," + (nowTime-endTime) + "," + mode +"\n";
+    }
+    // dpoint0 = point;
+
+    console.log("count-previous: " + positionPrevX + ", " + positionPrevY);
+
+    // console.log("count:" + count + "\ngap：" + gapX + ", " + gapY);
+    
+    // console.log("count-previous: " + positionPrevX + ", " + positionPrevY);
+    // console.log("count-current: " + currentX + ", " + currentY);
+
+    positionPrevX = currentX;
+    positionPrevY = currentY;
+
+    preGapX = gapX;
+    preGapY = gapY;
+    preGap = gap;
+
+    preVelX = velX;
+    preVelY = velY;
+    preVel = vel;
+    
+    count++;
+
+
+    // var start = Date.now();
+    // var now = performance.now();
+    // console.log("時間1：" + start);
+    // console.log("時間2：" + now);
+  
+
+    // 50ミリ秒後に再度関数を実行
+    setTimeout(speedCount, 10);
+
+  }
+  
+
+  
+  
+
+
+
+  // ページ切替用ボタン
+  const prevButton = $('#prevPage');
+  const nextButton = $('#nextPage');
+  // ページ数によって変更
+  let currentPage = 1;
+  const totalPages = 4;
+  prevButton.on('click', prevPage);
+  nextButton.on('click', nextPage);
+
+  function nextPage() {
+    if (currentPage < totalPages) {
+      currentPage++;
+      // resetCoordinates();
+      clearCanvas();
+      drawPage(currentPage);
+    }
+  }
+  function prevPage() {
+    if (currentPage > 1) {
+      currentPage--;
+      // resetCoordinates();
+      clearCanvas();
+      drawPage(currentPage);
+    }
+  }
+
+
+
+  // モード切替用ボタン
+  const penButton = $('#pen-button');
+  const pageButton = $('#page-button');
+  penButton.on('click', changePen);
+  pageButton.on('click', changePage);
+  function changePen() {
+    mode = "pen";
+    // ボタン色変更
+    penButton.css('background-color', '#d0d0d0');
+    pageButton.css('background-color', '#fff');
+  }
+  function changePage() {
+    mode = "page";
+    // ボタン色変更
+    pageButton.css('background-color', '#d0d0d0');
+    penButton.css('background-color', '#fff');
+  }
+
+
+  // csvファイルダウンロード用ボタン
+  $('#dl-button').on('click', function() {
+    download();
+  });
+  function download() {
+    // csvファイルへの書き出し
+    var blob = new Blob([str],{type:"text/csv"}); //配列に上記の文字列(str)を設定
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = "test.csv";
+    link.click();
+  
+    // 初速度、初筆圧など
+    var blob2 = new Blob([str0],{type:"text/csv"}); //配列に上記の文字列(str)を設定
+    var link2 = document.createElement('a');
+    link2.href = URL.createObjectURL(blob2);
+    link2.download = "test0.csv";
+    link2.click();
+  }
+
+
+  
+  // 拡大・縮小
+  $('#zoomIn').on('click', function() {
+    if (scale < 2) {
+      scale += 0.1;
+      updateCanvas();
+    }
+  });
+
+  $('#zoomOut').on('click', function() {
+    if (scale > 0.5) {
+      scale -= 0.1;
+      updateCanvas();
+    }
+  });
+
+  function updateCanvas() {
+    const originalWidth = canvas.width;
+    const originalHeight = canvas.height;
+  
+    const scaledWidth = originalWidth * scale;
+    const scaledHeight = originalHeight * scale;
+  
+    tempCanvas.width = scaledWidth;
+    tempCanvas.height = scaledHeight;
+  
+    tempCtx.clearRect(0, 0, scaledWidth, scaledHeight);
+    tempCtx.drawImage(canvas, 0, 0, originalWidth, originalHeight, 0, 0, scaledWidth, scaledHeight);
+  
+    ctx.clearRect(0, 0, originalWidth, originalHeight);
+    ctx.drawImage(backgroundImage, 0, 0);
+    ctx.drawImage(tempCanvas, 0, 0);
+  }
+  
+
+
+  const pages = [
+    // { background: '#ff00ff', drawings: [] }, // ページ1のデータ
+    // { background: '#ffff00', drawings: [] }, // ページ2のデータ
+  	{ background: '/img/Textbook_page1.png', drawings: [] }, // ページ1のデータ
+    { background: '/img/Textbook_page2.png', drawings: [] }, // ページ2のデータ
+    { background: '/img/Textbook_page3.png', drawings: [] }, // ページ3のデータ
+    { background: '/img/Textbook_page4.png', drawings: [] }, // ページ4データ
+    // 他のページのデータも同様に追加
+  ];
+
+  // function resetCoordinates() {
+  //   currentX = 0;
+  //   currentY = 0;
+  // }
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function drawPage(pageIndex) {
+    const page = pages[pageIndex - 1];
+  
+    // 背景画像を読み込む
+    const backgroundImage = new Image();
+    backgroundImage.src = page.background;
+  
+    backgroundImage.onload = function() {
+      // 背景画像の縮小後の幅と高さを計算する
+      const scaledWidth = canvas.width * scale;
+      const scaledHeight = canvas.height * scale;
+
+      // 背景画像をキャンバスに描画する
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(backgroundImage, 0, 0, scaledWidth, scaledHeight);
+
+      // ページの描画内容を反映する
+      for (let i = 0; i < page.drawings.length; i++) {
+        const drawing = page.drawings[i];
+        const previousX = drawing.previousX * scale;
+        const previousY = drawing.previousY * scale;
+        const currentX = drawing.currentX * scale;
+        const currentY = drawing.currentY * scale;
+      
+        ctx.beginPath();
+        ctx.moveTo(previousX, previousY);
+        ctx.lineTo(currentX, currentY);
+        ctx.stroke();
+      }
+    };
+  }
+});
